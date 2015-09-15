7e25d925dce17e181a941ec489e207e37fcdfe36
sophannapel <sophannapel@gmail.com>
2015-09-14 20:59:12 -0500

    Database script update.

--- src/main/resources/Sqldumbs/mumscrum.sql
+++ src/main/resources/Sqldumbs/mumscrum.sql
@@ -11,17 +11,17 @@
  Target Server Version : 50622
  File Encoding         : utf-8
 
- Date: 09/13/2015 20:38:27 PM
+ Date: 09/14/2015 20:58:15 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `employee`
+--  Table structure for `Employee`
 -- ----------------------------
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE `employee` (
+DROP TABLE IF EXISTS `Employee`;
+CREATE TABLE `Employee` (
   `id` int(15) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(45) DEFAULT NULL,
   `lastname` varchar(25) DEFAULT NULL,
@@ -29,10 +29,18 @@ CREATE TABLE `employee` (
   `password` varchar(25) DEFAULT NULL,
   `roleId` int(15) DEFAULT NULL,
   `status` varchar(25) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `ROLE_EMP` (`roleId`),
   CONSTRAINT `ROLE_EMP` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `Employee`
+-- ----------------------------
+BEGIN;
+INSERT INTO `Employee` VALUES ('1', 'sophanna', 'pel', 'a', 'a', '1', 'active', null);
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `product`
@@ -51,7 +59,14 @@ CREATE TABLE `product` (
   KEY `STATUS_EMP` (`statusId`),
   CONSTRAINT `PRODUCT_EMP` FOREIGN KEY (`employeeId`) REFERENCES `employee` (`id`),
   CONSTRAINT `STATUS_EMP` FOREIGN KEY (`statusId`) REFERENCES `status` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `product`
+-- ----------------------------
+BEGIN;
+INSERT INTO `product` VALUES ('1', 'scrum', '2015-09-13 22:32:13', '2015-09-15 22:32:16', 'agile approach', '1', '1'), ('2', 'rup', '2015-09-14 01:50:30', '2015-09-16 01:50:34', 'rup approach', '1', '1');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `releaseBacklog`
@@ -78,7 +93,14 @@ CREATE TABLE `role` (
   `id` int(15) NOT NULL AUTO_INCREMENT,
   `name` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `role`
+-- ----------------------------
+BEGIN;
+INSERT INTO `role` VALUES ('1', 'Product owner'), ('2', 'Scrum master'), ('3', 'Developer'), ('4', 'Tester'), ('5', 'HR Admin');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `sprint`
